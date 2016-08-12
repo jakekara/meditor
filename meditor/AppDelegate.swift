82ad5f98b66daa326d3653b0cc0e9870632e3fc2
Jake Kara <jkara@g.harvard.edu>
2016-08-12 17:50:43 -0400

    Initial Commit

---
+++ meditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  meditor
+//
+//  Created by Jake Kara on 8/12/16.
+//  Copyright © 2016 Jake Kara. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ meditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  meditor
+//
+//  Created by Jake Kara on 8/12/16.
+//  Copyright © 2016 Jake Kara. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
